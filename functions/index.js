9ce3fe647f3f8a2bc518b2cb404fb5ce7bc9112c
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-01 20:25:59 +0000

    Added a basic calendar view.

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,15 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const emailDomain = '@latymer-upper.org';
+
+const emailRegex = new RegExp(emailDomain + '\\s*$');
+
+exports.disableNonDomainAccounts = functions.auth.user().onCreate((user) => {
+    let email = user.email;
+
+    if (emailRegex.test(email)) {
+        functions.app.admin.auth().updateUser(user.uid, {disabled: true})
+    }
+});
