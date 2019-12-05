9012f590a623fc6efc34104f9283796c983dbcc8
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-05 18:29:35 +0000

    Changed Sentry config to show the feedback form upon a crash.

--- app/src/index.js
+++ app/src/index.js
@@ -6,7 +6,16 @@ import * as Sentry from '@sentry/browser';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-Sentry.init({dsn: "https://393bee494b7242b6b21273b8787227c3@sentry.io/1844736"});
+Sentry.init({
+    dsn: "https://393bee494b7242b6b21273b8787227c3@sentry.io/1844736",
+    beforeSend(event, hint) {
+        // Check if it is an exception, and if so, show the report dialog
+        if (event.exception) {
+            Sentry.showReportDialog({eventId: event.event_id});
+        }
+        return event;
+    }
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
