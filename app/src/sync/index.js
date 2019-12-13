8eeaecc9b4f58364f87e2d6311e314585030f45f
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-13 18:50:41 +0000

    Edited `sync/index.js` to only import the needed parts of firebase.

--- app/src/sync/index.js
+++ app/src/sync/index.js
@@ -1,4 +1,7 @@
-import * as firebase from "firebase";
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
 import {Collection, Document, initFirestorter} from 'firestorter';
 
 let user = null;
