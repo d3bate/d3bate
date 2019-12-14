7e7827ad2d5ecb4211f95571a593a2c490d55227
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-14 17:19:58 +0000

    Removed spaces from deploy to firebase action.

--- app/src/views/Dashboard.js
+++ app/src/views/Dashboard.js
@@ -3,10 +3,16 @@ import React from 'react';
 class Dashboard extends React.Component {
     render() {
         return <>
-            <div className="center">
-                <h1>DEBATING.</h1>
-                <h2>SUPERCHARGED</h2>
+            <div className="center row">
+                <img className="col-25" width="25%"
+                     style={{verticalAlign: 'middle'}}
+                     src="https://rawcdn.githack.com/d3bate/d3bate/fe333a28717a3b1588d3abb5fe22311c49c95201/assets/illustrations/debator-and-podium.jpg"/>
+                <div className="col-75" style={{textAlign: 'left', paddingTop: "12%"}}>
+                    <h1>D3BATE</h1>
+                    <h3>A digital debating toolkit.</h3>
+                </div>
             </div>
+            <hr style={{width: "75%"}}/>
         </>
     }
 }
