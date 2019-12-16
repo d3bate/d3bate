872e40d48a3e33c2c0c3e6e479d7809de0888799
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-16 16:42:42 +0000

    Added basic database models (in Rust)

--- backend/src/models.rs
+++ backend/src/models.rs
@@ -1,4 +1,9 @@
-#[derive(Queryable)]
+use diesel::{self, prelude::*};
+use self::schema::users;
+use self::schema::users::dsl::{users as all_users};
+
+#[table_name = "users"]
+#[derive(Serialize, Queryable, Insertable, Debug, Clone)]
 pub struct User {
     pub id: i32,
     pub name: String,
@@ -6,3 +11,20 @@ pub struct User {
     pub email_verified: i32,
     pub password_hash: String,
 }
+
+#[table_name = "clubs"]
+#[derive(Serialize, Queryable, Insertable, Debug, Clone)]
+pub struct Club {
+    pub id: i32,
+    pub name: String,
+    pub owner: i32,
+}
+
+#[table_name = "clubmemberships"]
+#[derive(Serialize, Queryable, Insertable, Debug, Clone)]
+pub struct ClubMemberships {
+    pub id: i32,
+    pub user: i32,
+    pub club: i32,
+}
+
