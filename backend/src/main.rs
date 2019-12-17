b36b57ac6490d9515aa80ef07bee2f2783a3f789
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-17 15:27:15 +0000

    Added ability to insert users into the database.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,26 +1,63 @@
-#![feature(proc_macro_hygiene, decl_macro, proc_macro_derive)]
+#![feature(proc_macro)]
 
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
-extern crate r2d2;
-extern crate r2d2_diesel;
-extern crate rocket;
-extern crate rocket_contrib;
-#[macro_use]
-extern crate serde_derive;
 
-mod users;
+use std::env;
+use std::io::{Read, stdin};
+
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use dotenv::dotenv;
+
+use models::User;
+use schema::users;
+
 mod schema;
+mod models;
 
-#[database("sqlite_db")]
-struct SQLiteDbConn(diesel::SqliteConnection);
+#[derive(Insertable)]
+#[table_name = "users"]
+pub struct NewUser<'a> {
+    pub name: &'a str,
+    pub email: &'a str,
+    pub email_verified: &'a i32,
+    pub password_hash: &'a str,
+}
 
-fn rocket() -> rocket::Rocket {
-    rocket::ignite()
-        .attach(SQLiteDbConn::fairing())
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+        .expect(&format!("Error connection to {}", database_url))
+}
+
+pub fn create_user<'a>(conn: SqliteConnection, name: &'a str, email: &'a str, password_hash: &'a str) -> usize {
+    use schema::users;
+    let email_verified: i32 = 0;
+    let new_user = NewUser {
+        name,
+        email,
+        email_verified: &email_verified,
+        password_hash,
+    };
+
+    diesel::insert_into(users::table).values(&new_user).execute(&conn).expect("Could not insert into table.")
 }
 
 fn main() {
-    rocket().launch();
+    use schema::users::dsl::*;
+    let connection = establish_connection();
+
+    println!("Username: ");
+    let mut username = String::new();
+    stdin().read_line(&mut username).unwrap();
+    let username = &username[..(username.len() - 1)];
+    let mut mail = String::new();
+    stdin().read_line(&mut mail).unwrap();
+    let mail = &mail[..(username.len() - 1)];
+    let user = create_user(connection, username, mail, &String::from("not a hash"));
+    println!("Saved user {} with id {}", username, user)
 }
--- backend/src/models.rs
+++ backend/src/models.rs
@@ -1,7 +1,6 @@
 use diesel::{self, prelude::*};
 
 #[derive(Queryable)]
-#[table_name = "users"]
 pub struct User {
     pub id: i32,
     pub name: String,
@@ -10,16 +9,8 @@ pub struct User {
     pub password_hash: String,
 }
 
-#[derive(Insertable)]
-#[table_name = "users"]
-pub struct InsertableUser {
-    pub name: String,
-    pub email: String,
-    pub email_verified: i32,
-    pub password_hash: String,
-}
 
-impl InsertableUser {
+/*impl InsertableUser {
     pub fn from_user(user: User) -> InsertableUser {
         InsertableUser {
             name: user.name,
@@ -28,18 +19,16 @@ impl InsertableUser {
             password_hash: user.password_hash,
         }
     }
-}
+}*/
 
-#[table_name = "clubs"]
-#[derive(Serialize, Queryable, Insertable, Debug, Clone)]
+#[derive(Queryable)]
 pub struct Club {
     pub id: i32,
     pub name: String,
     pub owner: i32,
 }
 
-#[table_name = "clubmemberships"]
-#[derive(Serialize, Queryable, Insertable, Debug, Clone)]
+#[derive(Queryable)]
 pub struct ClubMemberships {
     pub id: i32,
     pub user: i32,
