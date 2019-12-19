73718778bc800bfc4a031a269fffbfa2fad78c8a
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-19 16:28:26 +0000

    Added area under construction image.

--- app/src/views/TakeRegister.js
+++ app/src/views/TakeRegister.js
@@ -9,9 +9,18 @@ export const TakeRegister = observer(class TakeRegister extends React.Component 
 
     render() {
         return <>
-            <div className="row">
-                <div className="col-25" style={{textAlign: 'center'}}>
+            <div className="row-center-justified">
+                <div className="col-100" style={{textAlign: 'center'}}>
                     <h5>AREA UNDER CONSTRUCTION.</h5>
+                </div>
+
+            </div>
+            <div className="row-center-justified">
+                <div className="col-100">
+                    <img
+                        src="https://rawcdn.githack.com/d3bate/d3bate/a4c2fc222cc0bcdb53e5f1ede0453d59285712e7/assets/illustrations/area-under-construction.png"
+                        alt="Area under construction." width="25%"
+                        style={{marginLeft: 'auto', marginRight: 'auto', display: 'block'}}/>
                 </div>
             </div>
         </>
