f9548c735b174691923547fe2083228986e8059c
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-28 16:12:21 +0000

    Added a view club dashboard for administrators.

--- app/src/views/club/AdminClubView.js
+++ app/src/views/club/AdminClubView.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import {observer} from "mobx-react";
 import {debatingClub} from "../../sync/models/club";
+import {Pane, Card, Button, minorScale} from "evergreen-ui";
+import {appState} from "../../sync/models";
 
 
-class AdminClubView extends React.Component {
+const AdminClubView = observer(class AdminClubView extends React.Component {
     render() {
         console.log(debatingClub);
         if (!debatingClub.club)
             return <p>Loading</p>;
         return <>
-            <h6>Club ID: {debatingClub.club.id}</h6>
+            <Pane margin={'10px'} padding={'10px'}>
+                <h6>Hi {appState.user.displayName}. You are an administrator of the {debatingClub.club.clubID} debating
+                    club. </h6>
+            </Pane>
+            <Card elevation={1} padding={'10px'} margin={'10px'}>
+                <h6>Quick actions</h6>
+                <p>Be warned – none of these work.</p>
+                <Button marginRight={minorScale(3)}>Edit the calendar</Button>
+                <Button marginRight={minorScale(3)}>View the calendar</Button>
+                <Button marginRight={minorScale(3)}>Take a register</Button>
+            </Card>
         </>
     }
-}
+});
 
 export {AdminClubView};
