db11223115631ea1323bacc5db1f77cde4a02b39
Teymour Aldridge <teymour.aldridge@icloud.com>
2019-12-29 14:36:49 +0000

    Updated EditCalendar to show only the relevant dates for each month.

--- app/src/views/EditCalendar.js
+++ app/src/views/EditCalendar.js
@@ -5,6 +5,7 @@ import {debatingClub} from "../sync/models/club";
 import {Redirect} from "react-router-dom";
 import {firebase} from "../sync";
 import * as moment from "moment";
+import {Button, Card, minorScale, TextInput, Select} from "evergreen-ui";
 
 const eventTypes = [
     'training', 'tournament', 'friendly'
@@ -30,9 +31,10 @@ const ViewEvent = observer(class ViewEvent extends React.Component {
     render() {
         if (this.state.edit)
             return <>
-                <div>
+                <Card marginTop={'10px'} background="#E4E7EB" elevation={2} padding={minorScale(3)}
+                      margin={minorScale(3)}>
                     <form>
-                        <select value={this.state.type} onChange={(event) => {
+                        <Select value={this.state.type} onChange={(event) => {
                             this.setState({type: event.target.value})
                         }}>
 
@@ -40,42 +42,46 @@ const ViewEvent = observer(class ViewEvent extends React.Component {
                                 return <option value={eventType} key={index}>{eventType}</option>
                             })}
 
-                        </select>
-
-                        <input type="number" value={this.state.year} className="authInput" onChange={(event) => {
+                        </Select>
+                        <br/>
+                        <TextInput type="number" value={this.state.year} className="authInput" onChange={(event) => {
                             this.setState({year: event.target.value})
                         }}/>
-
-                        <input type="number" className="authInput" value={this.state.month} onChange={(event) => {
+                        <br/>
+                        <TextInput type="number" className="authInput" value={this.state.month} onChange={(event) => {
                             this.setState({month: event.target.value})
                         }} min={1} max={12}/>
-
-                        <input type="number" className="authInput" value={this.state.day} onChange={(event) => {
+                        <br/>
+                        <TextInput type="number" className="authInput" value={this.state.day} onChange={(event) => {
                             this.setState({day: event.target.value})
                         }} min={1} max={31}/>
-
-                        <input type={'submit'}/>
+                        <br/>
+                        <Button intent="success" margin={minorScale(3)} iconAfter="tick-circle">
+                            Save
+                        </Button>
+                        <Button iconAfter="ban-circle" intent="danger" onClick={(event) => {
+                            event.preventDefault();
+                            this.setState({edit: false})
+                        }
+                        }>Cancel changes
+                        </Button>
                     </form>
-                    <button className="authSubmit" onClick={(event) => {
-                        event.preventDefault();
-                        this.setState({edit: false})
-                    }
-                    }>Cancel changes
-                    </button>
-                </div>
+
+                </Card>
             </>;
         else
             return <>
-                <div className="card">
-                    <p>{this.getDay()}</p>
-                    <p>{this.state.year}/{this.state.month}/{this.state.day}</p>
-                    <button className="authSubmit" onClick={(event) => {
+                <Card marginTop={'10px'} background="#E4E7EB" elevation={2} padding={minorScale(3)}>
+                    <p>Event type: {this.state.type}</p>
+                    <p>On: {this.getDay()}</p>
+                    <p>Date: {this.state.year}/{this.state.month}/{this.state.day}</p>
+                    <Button iconAfter="edit" className="authSubmit" onClick={(event) => {
                         event.preventDefault();
                         this.setState({edit: true})
                     }
                     }>Edit
-                    </button>
-                </div>
+                    </Button>
+                </Card>
             </>
     }
 });
@@ -103,27 +109,39 @@ class AddEvent extends React.Component {
     }
 
     render() {
-        return <div className="">
+        return <Card marginTop={'10px'} background="#E4E7EB" elevation={2} padding={minorScale(3)}
+                     marginBottom={'10px'}>
+            <h6>Add event</h6>
             <form onSubmit={this.submitForm}>
-                <select onChange={(event) => {
+                <label>Event type: </label>
+                <Select onChange={(event) => {
                     this.setState({type: event.target.value})
                 }} value={this.state.type}>
                     {eventTypes.map((eventType, index) => {
                         return <option value={eventType} key={index}>{eventType}</option>
                     })}
-                </select>
-                <input type="number" value={this.state.year} onChange={(event) => {
+                </Select>
+                <br/>
+                <label>Year:</label>
+                <TextInput type="number" value={this.state.year} onChange={(event) => {
                     this.setState({year: event.target.value})
-                }}/>
-                <input type="number" value={this.state.month} onChange={(event) => {
+                }} margin={minorScale(3)}/>
+                <br/>
+                <label>Month: </label>
+                <TextInput type="number" value={this.state.month} onChange={(event) => {
                     this.setState({month: event.target.value})
-                }} min={1} max={12}/>
-                <input type="number" value={this.state.day} onChange={(event) => {
+                }} min={1} max={12} margin={minorScale(3)}/>
+                <br/>
+                <label>Day: </label>
+                <TextInput type="number" value={this.state.day} onChange={(event) => {
                     this.setState({day: event.target.value})
-                }} min={1} max={31}/>
-                <input type="submit"/>
+                }} min={1} max={31} margin={minorScale(3)}/>
+                <br/>
+                <Button height={minorScale(10)} margin={minorScale(3)} iconAfter="tick-circle">
+                    Add event to calendar.
+                </Button>
             </form>
-        </div>
+        </Card>
     }
 }
 
@@ -135,13 +153,20 @@ const EditCalendar = observer(class EditCalendar extends React.Component {
         };
     }
 
+    filterCalendar() {
+        return calendar.events.filter(o => {
+            let date = moment(this.props.match.match.params.year + '-' + this.props.match.match.params.month + '-' + 1, 'YYYY-MM-DD');
+            let eventDate = new Date(o.startTime.seconds * 1000);
+            return eventDate > date.toDate() && eventDate < date.add(1, 'month').toDate();
+        })
+    }
+
     render() {
         if (!JSON.parse(localStorage.getItem('user'))) {
             return <Redirect to='/login'/>
         }
         return <div className="container">
-
-            {calendar.events.map((event, index) => {
+            {this.filterCalendar().map((event, index) => {
                 let date = new Date(event.startTime.seconds * 1000);
                 return <ViewEvent type={event.type} year={date.getFullYear()} month={date.getMonth() + 1}
                                   day={date.getDate()} key={index}/>
