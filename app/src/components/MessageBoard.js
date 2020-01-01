902c59cdd408e88fba5c7234e34eee13b3fc7240
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-01-01 11:07:20 +0000

    Switched `MessageBoard` alerts to use evergreen's `Alert` component instead of the `Card` component.

--- app/src/components/MessageBoard.js
+++ app/src/components/MessageBoard.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import {messages} from "../sync/messages";
 import {observer} from "mobx-react";
-import {Card} from "evergreen-ui";
+import {Alert, majorScale} from "evergreen-ui";
 
 const MessageBoard = observer(class MessageBoard extends React.Component {
     render() {
         return <>
             {messages.messages.map(message => {
-                return <Card>
+                return <Alert appearance="card" intent={message.category} marginBottom={majorScale(4)}>
                     <p>{message.title}</p>
                     <p>{message.body}</p>
                     <button onClick={() => {
                         messages.deleteMessage(message.id)
                     }}>Dismiss message
                     </button>
-                </Card>
+                </Alert>
             })}
         </>
     }
