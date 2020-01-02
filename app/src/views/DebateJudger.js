e309faa3a93a21b32208f5864da7c8ddc5436fcd
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-01-02 10:04:45 +0000

    Removed unnecessary calls to mobx's `observer` function in the `DebateJudger` component. =

--- app/src/views/DebateJudger.js
+++ app/src/views/DebateJudger.js
@@ -4,7 +4,7 @@ import {Combobox, Button, TextInput} from "evergreen-ui";
 import {clubUsers} from "../sync/models/clubUsers";
 import ReactMarkdown from 'react-markdown';
 
-const SpeakerConfig = observer((props) => {
+const SpeakerConfig = props => {
     const [edit, setEdit] = useState(false);
     if (edit)
         return <>
@@ -22,9 +22,9 @@ const SpeakerConfig = observer((props) => {
                 setEdit(true)
             }}>Edit</Button>
         </>
-});
+};
 
-const SpeakerBox = observer(props => {
+const SpeakerBox = props => {
     const [edit, setEdit] = useState(false);
     if (edit)
         return <>
@@ -36,20 +36,28 @@ const SpeakerBox = observer(props => {
             <ReactMarkdown source={props.speaker}/>
             <Button onClick={e => setEdit(true)} iconAfter="pencil">Edit</Button>
         </>
-});
+};
 
-const Speaker = observer(props => {
+const Speaker = props => {
     return <>
         <SpeakerConfig setSpeaker={props.setSpeaker} speaker={speaker}/>
         <SpeakerBox updateSpeaker={props.updateSpeaker} speaker={speakerNotes}/>
     </>
-});
+};
 
-const DebateJudger = observer(props =>
-    <>
-        <h5>Judge a debate</h5>
-
+const Team = props => {
+    return <>
+        <Speaker setSpeaker={props.setSpeaker1} speaker={props.speaker1} updateSpeaker={props.updateSpeaker1}
+                 speakerNotes={props.speaker1Notes}/>
+        <Speaker setSpeaker={props.setSpeaker2} speaker={props.speaker2} updateSpeaker={props.updateSpeaker2}
+                 speakerNotes={props.speaker2Notes}/>
     </>
-);
+};
+
+const DebateJudger = observer(props => {
+    return <>
+        <h5>Judge a debate</h5>
+    </>;
+});
 
 export {DebateJudger}
