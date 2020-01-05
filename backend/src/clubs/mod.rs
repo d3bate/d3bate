d35fc094a8450680727b58e998ae0f0e244b96be
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-01-05 17:12:13 +0000

    Refactored macros to use only a single macro to create all the routes.

--- backend/src/clubs/mod.rs
+++ backend/src/clubs/mod.rs
@@ -1,10 +1,39 @@
-macro_rules! generate_route {
-    (create => $create_function:expr) => {
-    }
-    (read => $create_function:expr) => {
-    }
-    (update => $create_function:expr) => {
-    }
-    (delete => $create_function:expr) => {
-    }
+use actix_web::post;
+use diesel::prelude::{Queryable, Insertable};
+
+macro_rules! crud {
+    (struct $name:ident { $($fname: ident : $ftype:ty),* }) => {
+        #[derive(Queryable)]
+        #[table_name="$name"]
+        struct $name {
+            $($fname: $ftype),*
+        }
+
+        #[derive(Insertable)]
+        #[table_name="$name"]
+        struct Insertable$name<'a> {
+            $($fname: &'a $ftype),*
+        }
+
+        fn add_$name() {}
+
+        fn update_$name() {}
+
+        fn delete_$name() {}
+
+        fn read_$name() {}
+
+
+        #[post("/"$name"/create")]
+        pub fn create_$name() {}
+
+        #[post("/"$name"/read")]
+        pub fn read_$name() {}
+
+        #[post("/"$name"/update")]
+        pub fn update_$name() {}
+
+        #[post("/"$name"/delete")]
+        pub fn read_$name() {}
+    };
 }
