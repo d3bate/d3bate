040fccc52b6f2d250850f55736a5d823261cf086
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-01-06 16:14:47 +0000

    Added structs to decode API calls.

--- backend/src/clubs/mod.rs
+++ backend/src/clubs/mod.rs
@@ -16,18 +16,38 @@ macro_rules! crud {
             $($fname: &'a $ftype),*
         }
 
-        fn add_$name(conn: &SqliteConnection, $($fname: &'a $ftype),*) {
+        fn add_$name(conn: &SqliteConnection, $($fname: &'a $ftype),*) -> usize {
             let new_$name = Insertable$name {
                 $($fname: $ftype),*
             }
             diesel::insert_into(table).values(&new_$name).execute(conn).expect("Could not insert that user into the table.")
         }
 
-        fn update_$name() {}
+        fn update_$name(conn: &SqliteConnection) {}
 
         fn delete_$name() {}
 
         fn read_$name() {}
+
+        #[derive(Deserialize)]
+        struct Create$name {
+            $($fname: $ftype),*
+        }
+
+        #[derive(Deserialize)]
+        struct Read$name {
+            query: String,
+        }
+
+        #[derive(Deserialize)]
+        struct Update$name {
+            $($name: $ftpye),*
+        }
+
+        #[derive(Deserialize)]
+        struct Delete$name {
+            id: usize
+        }
 
 
         #[post("/"$name"/create")]
