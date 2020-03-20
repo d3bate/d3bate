d5443d870777331ee90f3d60b66b754eade00a56
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-03-20 18:16:13 +0000

    Set a custom location for session data.

--- backend/app.py
+++ backend/app.py
@@ -16,15 +16,19 @@ session = Session()
 
 def create_app() -> Flask:
     app = Flask(__name__)
+
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     app.config["SESSION_REDIS"] = os.environ.get("REDIS_URL")
-    app.config["SESSION_TYPE"] = "filesystem" if os.environ.get("FLASK_ENV") == "development" else "redis"
+    app.config["SESSION_TYPE"] = "filesystem"
+    app.config["SESSION_FILE_DIR"] = "session-data"
+
     db.init_app(app)
+    session.init_app(app)
     migrate.init_app(app, db)
     jwt_manager.init_app(app)
-    session.init_app(app)
     socketio.init_app(app)
+
     from auth import auth_blueprint
     app.register_blueprint(auth_blueprint)
     from club import club_blueprint
