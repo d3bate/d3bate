c3ccc2150c772bdc6b2bd9057a741c809bbced96
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-03-21 08:13:18 +0000

    Added skeleton view components.

--- client/views/ClubTrainingSessions.js
+++ client/views/ClubTrainingSessions.js
@@ -1,2 +1,18 @@
+import React from "react";
+import {View} from "react-native";
 
+class ClubTrainingSessions extends React.Component {
+    constructor(props) {
+        super(props);
 
+    }
+
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+
+}
---
+++ client/views/EditClubTrainingSessions.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {View} from "react-native";
+
+class EditClubTrainingSessions extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <View>
+
+            </View>
+        );
+    }
+
+}
