8ee8d417cb08e9a304f2169c30464c73a2b669c6
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-03-21 07:58:29 +0000

    Added action `RECEIVE_CREATE_CLUB`

--- client/state/index.js
+++ client/state/index.js
@@ -1,6 +1,3 @@
-import {createStore} from "redux";
-import thunkMiddleware from "redux-thunk";
-
 const REQUEST_JWT = "REQUEST_JWT";
 const RECEIVE_JWT = "RECEIVE_JWT";
 const ADD_CREDENTIALS = "ADD_CREDENTIALS";
@@ -14,16 +11,16 @@ function authReducer(state = {
 }, action) {
     switch (action.type) {
         case REQUEST_JWT:
-            return Object.assign(state, {}, {
+            return Object.assign({}, state, {
                 fetchingCredentials: true
             });
         case RECEIVE_JWT:
-            return Object.assign(state, {}, {
+            return Object.assign({}, state, {
                 jwt: action.data.token,
                 jwtLastFetched: new Date().getSeconds()
             });
         case ADD_CREDENTIALS:
-            return Object.assign(state, {}, {
+            return Object.assign({}, state, {
                 identifier: action.data.username,
                 password: action.data.password
             });
@@ -38,25 +35,30 @@ const DELETE_MESSAGE = "DELETE_MESSAGE";
 function messagesReducer(state = {messages: []}, action) {
     switch (action.type) {
         case ADD_MESSAGE:
-            return Object.assign(state, {}, {messages: [{id: state.messages.length, ...action.data}, ...state.messages]});
+            return Object.assign({}, state, {messages: [{id: state.messages.length, ...action.data}, ...state.messages]});
         case DELETE_MESSAGE:
-            return Object.assign(state, {}, {messages: state.messages.filter(o => o.id !== action.data.id)})
+            return Object.assign({}, state, {messages: state.messages.filter(o => o.id !== action.data.id)})
     }
 }
 
 const REQUEST_CLUB_DATA = "REQUEST_CLUB_DATA";
 const RECEIVE_CLUB_DATA = "GET_CLUBS";
+const RECEIVE_CREATE_CLUB = "RECEIVE_CREATE_CLUB";
 
 function clubsReducer(state = {fetching: false, clubs: []}, action) {
     switch (action.type) {
         case REQUEST_CLUB_DATA:
-            return Object.assign(state, {}, {
+            return Object.assign({}, state, {
                 fetching: true
             });
         case RECEIVE_CLUB_DATA:
-            return Object.assign(state, {}, {
+            return Object.assign({}, state, {
                 fetching: false,
                 clubs: action.data
+            });
+        case RECEIVE_CREATE_CLUB:
+            return Object.assign({}, state, {
+                clubs: [action.data, ...state.clubs]
             });
         default:
             return state
@@ -70,7 +72,7 @@ const DELETE_TRAINING_SESSION = "DELETE_TRAINING_SESSION";
 function trainingSessionReducer(fetching: false, adding: true, updating: false, state = {trainingSessions: []}, action) {
     switch (action.type) {
         case ADD_TRAINING_SESSION:
-            return Object.assign(state, {}, {
+            return Object.assign({}, state, {
                 adding: false,
                 trainingSessions: [action.data, ...state.trainingSessions]
             });
@@ -78,12 +80,12 @@ function trainingSessionReducer(fetching: false, adding: true, updating: false, 
             let sessionItem = state.trainingSessions.findIndex(o => o.id === action.data.id);
             let clonedList = [...state.trainingSessions];
             clonedList[sessionItem] = {...action.data.update, ...clonedList[sessionItem]};
-            return Object.assign(state, {}, {
+            return Object.assign({}, state, {
                 updating: false,
                 trainingSessions: clonedList
             });
         case DELETE_TRAINING_SESSION:
-            return Object.assign(state, {}, {
+            return Object.assign({}, state, {
                 trainingSessions: state.trainingSessions.filter(o => o.id !== action.data.id)
             });
         default:
