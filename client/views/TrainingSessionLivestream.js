a512fc774c5ff4472313ad7fce83d8a0bd4ec1d4
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-03-24 14:37:40 +0000

    Added face detection code to the `TrainingSessionLivestream` component.

--- client/views/TrainingSessionLivestream.js
+++ client/views/TrainingSessionLivestream.js
@@ -1,5 +1,8 @@
 import React from "react";
 import {View} from "react-native";
+import {Camera} from "expo-camera";
+import * as FaceDetector from "expo-face-detector";
+
 
 class TrainingSessionLivestream extends React.Component {
     constructor(props) {
@@ -13,7 +16,17 @@ class TrainingSessionLivestream extends React.Component {
     render() {
         return (
             <View>
+                <Camera zoom={} ratio={} focusDepth={} onMountError={} pictureSize={} onCameraReady={} useCamera2Api={}
+                        onBarCodeScanned={} barCodeScannerSettings={} onFacesDetected={faces => {
 
+                }} faceDetectorSettings={{
+                    mode: FaceDetector.Constants.Mode.fast,
+                    detectLandmarks: FaceDetector.Constants.Landmarks.all,
+                    runClassifications: FaceDetector.Constants.Classifications.all,
+                    minDetectionInterval: 100,
+                    tracking: true
+                }} type={}
+                        flashMode={} videoStabilizationMode={} whiteBalance={} autoFocus={} Readonly={} name={}/>
             </View>
         );
     }
