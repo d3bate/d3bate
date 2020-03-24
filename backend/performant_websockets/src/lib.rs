f152aab08570f2ae93461344891036a90b3daa6b
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-03-24 16:15:59 +0000

    Added some skeleton code for the websockets.

--- backend/performant_websockets/src/lib.rs
+++ backend/performant_websockets/src/lib.rs
@@ -1,3 +1,33 @@
+use std::collections::HashMap;
+
+use actix::{Actor, StreamHandler};
+use actix_web_actions::ws;
+
+struct ReceiveVideo;
+
+impl Actor for ReceiveVideo {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ReceiveVideo {
+    fn handle(&mut self) {}
+}
+
+pub struct PerformantWebsockets {
+    sessions: HashMap<String, String>
+}
+
+impl PerformantWebsockets {
+    pub fn new() -> Self {
+        Self {
+            sessions: HashMap::new();
+        }
+    }
+
+    pub fn run() {}
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
