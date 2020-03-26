8df46957146fafc434a555b6d0354bca1e97923d
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-03-26 12:17:41 +0000

    Added skeleton register view.

---
+++ client/views/Register.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {Button, Text, TextInput, View} from "react-native";
+import axios from "axios";
+import {backendURL} from "../constants";
+import {connect} from "react-redux";
+import {Redirect} from "../routing/routing";
+
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            username: "",
+            email: "",
+            password: "",
+            passwordConfirmation: ""
+        };
+        this.register = this.register.bind(this);
+    }
+
+    register() {
+        axios.post(`${backendURL}/auth/register`, {
+            name: this.state.name,
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password,
+            redirect: null
+        })
+            .then(result => result.json)
+            .then(json => {
+                if (json["type"] === "success") {
+
+                }
+            })
+    }
+
+    render() {
+        if (this.auth.jwt)
+            return <Redirect to="/"/>;
+        return (
+            <View>
+                <Text>Register</Text>
+                <TextInput placeholder="Name" onTextChange={value => this.setState({name: value})}/>
+                <TextInput placeholder="Username" onTextChange={value => this.setState({username: value})}/>
+                <TextInput placeholder="Email" onTextChange={value => this.setState({email: value})}/>
+                <TextInput placeholder="Password" onTextChange={value => this.setState({password: value})}/>
+                <TextInput placeholder="Password confirmation"
+                           onTextChange={value => this.setState({passwordConfirmation: value})}/>
+                <Button title="Register" onPress={this.register}/>
+            </View>
+        );
+    }
+
+}
+
+export default connect((state, ownProps) => {
+    return {
+        auth: state.auth,
+        ...ownProps
+    }
+})
