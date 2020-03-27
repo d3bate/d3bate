00ee63d357844b2ed25c9f7fbc367563c13ebfd4
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-03-27 15:54:17 +0000

    Updated testing script.

--- backend/tests.sh
+++ backend/tests.sh
@@ -2,6 +2,7 @@
 . venv/bin/activate
 export FLASK_APP=app:create_app
 export DATABASE_URL=sqlite:///test.db
+export TESTING=true
 export SECRET_KEY=afghrbej3424thnr1452@lafd2t.ewgrf4t4njhnr-gsfdjknbmv
 flask db upgrade
 python -m unittest tests.py
