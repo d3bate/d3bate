fd5033612d96b06f925cf7770d6c23a42fa90bb9
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-03-27 10:09:38 +0000

    Added flask mail configuration.

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
@@ -13,6 +14,7 @@ from websockets import socketio
 migrate = Migrate()
 jwt_manager = JWTManager()
 session = Session()
+mail = Mail()
 
 
 def create_app() -> Flask:
@@ -24,12 +26,20 @@ def create_app() -> Flask:
     app.config["SESSION_REDIS"] = os.environ.get("REDIS_URL")
     app.config["SESSION_TYPE"] = "filesystem"
     app.config["SESSION_FILE_DIR"] = "session-data"
+    app.config["MAIL_DEFAULT_SENDER"] = "d3bate (do not reply) <bureaucrat@debating.web.app>" if not os.environ.get(
+        "MAIL_SENDER") else os.environ.get("MAIL_SENDER")
+    if os.environ.get("FLASK_ENV") != "development":
+        app.config["MAIL_SERVER"] = os.environ.get("MAILGUN_SMTP_SERVER")
+        app.config["MAIL_PORT"] = os.environ.get("MAILGUN_SMTP_PORT")
+        app.config["MAIL_USERNAME"] = os.environ.get("MAILGUN_SMTP_LOGIN")
+        app.config["MAIL_PASSWORD"] = os.environ.get("MAILGUN_SMTP_PASSWORD")
 
     db.init_app(app)
     session.init_app(app)
     migrate.init_app(app, db)
     jwt_manager.init_app(app)
     socketio.init_app(app)
+    mail.init_app(app)
 
     from auth import auth_blueprint
     app.register_blueprint(auth_blueprint)
