769bfd5ad717a43d9dfa89cd7236eebb57522d0d
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-03-27 09:49:40 +0000

    Updated styling of the message board.

--- client/components/Messages.js
+++ client/components/Messages.js
@@ -8,42 +8,30 @@ import {Ionicons} from "@expo/vector-icons";
 class Messages extends React.Component {
     render() {
         return (
-            this.props.messages.length > 0 ? <View>
-                <View
-                    style={{
-                        borderBottomColor: 'black',
-                        borderBottomWidth: 1,
-                    }}
-                />
-                <Text style={{fontSize: 24, paddingBottom: 10}}>Messages</Text>
-                {this.props.messages.map((message, messageIndex) => {
-                    return <View
-                        style={{
-                            backgroundColor: message.type === "success" ? colours.tertiary : colours.danger,
-                            padding: 10,
-                            borderRadius: 3,
-                            maxWidth: 300,
-                            display: "flex",
-                            flexDirection: "row"
-                        }}
-                        key={messageIndex}>
-                        <TouchableOpacity onPress={() => {
-                            this.props.deleteMessage(message.id)
-                        }} style={{marginRight: 5, paddingTop: 1}}>
-                            <Ionicons name="md-remove-circle" size={20} color="white"/>
-                        </TouchableOpacity>
-                        <Text style={{color: "white", paddingTop: 3}}>{message.message}</Text>
-                        <Text style={{color: "white", paddingTop: 3}}>{message.suggestion}</Text>
-                    </View>
-                })}
-                <View
-                    style={{
-                        borderBottomColor: 'black',
-                        borderBottomWidth: 1,
-                        marginTop: 5
-                    }}
-                />
-            </View> : null
+            this.props.messages.length > 0 ?
+                <View style={{marginBottom: 5, backgroundColor: "lightgrey", padding: 5, borderRadius: 3}}>
+                    <Text style={{fontSize: 24, paddingBottom: 10}}>Messages</Text>
+                    {this.props.messages.map((message, messageIndex) => {
+                        return <View
+                            style={{
+                                backgroundColor: message.type === "success" ? colours.tertiary : colours.danger,
+                                padding: 10,
+                                borderRadius: 3,
+                                maxWidth: 300,
+                                display: "flex",
+                                flexDirection: "row"
+                            }}
+                            key={messageIndex}>
+                            <TouchableOpacity onPress={() => {
+                                this.props.deleteMessage(message.id)
+                            }} style={{marginRight: 5, paddingTop: 1}}>
+                                <Ionicons name="md-remove-circle" size={20} color="white"/>
+                            </TouchableOpacity>
+                            <Text style={{color: "white", paddingTop: 3}}>{message.message}</Text>
+                            <Text style={{color: "white", paddingTop: 3}}>{message.suggestion}</Text>
+                        </View>
+                    })}
+                </View> : null
         );
     }
 }
