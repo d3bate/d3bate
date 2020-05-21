247ed07cf3577f959942d9daac006abbb42030a2
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-05-21 14:20:01 +0100

    Move duplicate style declarations into `linkStyles`

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from "react";
-import {Text, TouchableOpacity, View} from "react-native";
-import {Ionicons} from "@expo/vector-icons";
-import {colours} from "../styles";
-import {connect} from "react-redux";
-import {Link} from "../routing/routing";
+import { Text, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { colours } from "../styles";
+import { connect } from "react-redux";
+import { Link } from "../routing/routing";
+
+const linkStyles = {
+    fontSize: 24,
+    color: "white",
+    paddingLeft: 10,
+    paddingTop: 2
+};
 
 class Navbar extends React.Component {
     constructor(props) {
@@ -17,51 +24,41 @@ class Navbar extends React.Component {
         if (this.state.open)
             return <>
                 <View
-                    style={{backgroundColor: colours.primary, display: "flex", flexDirection: "row", padding: 10}}>
-                    <TouchableOpacity style={{display: "flex", flexDirection: "row"}} onPress={press => {
-                        this.setState({open: false})
+                    style={{ backgroundColor: colours.primary, display: "flex", flexDirection: "row", padding: 10 }}>
+                    <TouchableOpacity style={{ display: "flex", flexDirection: "row" }} onPress={press => {
+                        this.setState({ open: false })
                     }}>
-                        <Ionicons name="md-close" size={32} color="white"/>
-                        <Text style={{fontSize: 24, color: "white", paddingLeft: 10, paddingTop: 2}}>close</Text>
+                        <Ionicons name="md-close" size={32} color="white" />
+                        <Text style={linkStyles}>close</Text>
                     </TouchableOpacity>
 
                 </View>
-                <View style={{backgroundColor: colours.primary, display: "flex", flexDirection: "column", padding: 10}}>
-                    <Link to="/" style={{textDecoration: 'none'}}><Text
-                        style={{fontSize: 24, color: "white", paddingLeft: 10, paddingTop: 2}}>Home</Text></Link>
+                <View style={{ backgroundColor: colours.primary, display: "flex", flexDirection: "column", padding: 10 }}>
+                    <Link to="/" style={{ textDecoration: 'none' }}><Text
+                        style={linkStyles}>Home</Text></Link>
                     {this.props.auth.jwt ? <>
-                            <Link to="/club" style={{textDecoration: 'none'}}><Text
-                                style={{fontSize: 24, color: "white", paddingLeft: 10, paddingTop: 2}}>Club</Text></Link>
-                            <Text style={{fontSize: 24, color: "white", paddingLeft: 10, paddingTop: 2}}>Calendar</Text>
-                            <Text style={{fontSize: 24, color: "white", paddingLeft: 10, paddingTop: 2}}>Join debate</Text>
-                        </> :
+                        <Link to="/club" style={{ textDecoration: 'none' }}><Text
+                            style={linkStyles}>Club</Text></Link>
+                        <Text style={linkStyles}>Calendar</Text>
+                        <Text style={linkStyles}>Join debate</Text>
+                    </> :
                         <>
-                            <Link to="/login" style={{textDecoration: 'none'}}><Text
-                                style={{
-                                    fontSize: 24,
-                                    color: "white",
-                                    paddingLeft: 10,
-                                    paddingTop: 2
-                                }}>Login</Text></Link>
-                            <Link to="/register" style={{textDecoration: 'none'}}><Text
-                                style={{
-                                    fontSize: 24,
-                                    color: "white",
-                                    paddingLeft: 10,
-                                    paddingTop: 2
-                                }}>Register</Text></Link>
+                            <Link to="/login" style={{ textDecoration: 'none' }}><Text
+                                style={linkStyles}>Login</Text></Link>
+                            <Link to="/register" style={{ textDecoration: 'none' }}><Text
+                                style={linkStyles}>Register</Text></Link>
                         </>}
                 </View>
             </>;
         else
             return (
-                <View style={{backgroundColor: colours.primary, display: "flex", flexDirection: "row", padding: 10}}>
+                <View style={{ backgroundColor: colours.primary, display: "flex", flexDirection: "row", padding: 10 }}>
                     <TouchableOpacity onPress={press => {
-                        this.setState({open: true})
+                        this.setState({ open: true })
                     }}>
-                        <Ionicons name="md-menu" size={32} color="white"/>
+                        <Ionicons name="md-menu" size={32} color="white" />
                     </TouchableOpacity>
-                    <Text style={{fontSize: 24, color: "white", paddingLeft: 10, paddingTop: 2}}>d3bate</Text>
+                    <Text style={linkStyles}>d3bate</Text>
                 </View>
             );
     }
