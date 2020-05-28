fb50ff39cf44577e0a1eb94514324c0f762a6fb0
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-05-28 11:24:45 +0100

    Remove Python backend.

--- backend/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source venv/bin/activate
-export SECRET_KEY=tewhjknfd2@r3wejkfnggdmnsr3t4jreth-3r0ew-fds0-+FSDgodjknb
-export DATABASE_URL=sqlite:///test.db
-export FLASK_ENV=development
-export TESTING=true
-flask db upgrade
-python app.py
--- backend/tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-. venv/bin/activate
-export FLASK_APP=app:create_app
-export DATABASE_URL=sqlite:///test.db
-export TESTING=true
-export SECRET_KEY=afghrbej3424thnr1452@lafd2t.ewgrf4t4njhnr-gsfdjknbmv
-flask db upgrade
-python -m unittest tests.py
-rm -rf test.db
